00295b5ed2cbdb62cfaf37ebad125d55123a3005
zombieyang <zombieyang@tencent.com>
2019-11-20 16:12:26 +0800

    chapter5

--- chapter5/play-page-enhance/boot.js
+++
@@ -1,8 +0,0 @@
-const nodeserver = require('./bff');
-
-nodeserver.listen(3000)
-
-require('./backend/comment-list')
-require('./backend/comment-praise')
-require('./backend/article')
-require('./backend/detail')
